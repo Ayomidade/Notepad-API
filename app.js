df9dbb3dd097252f2a88429b139eaf400d21091c
myk3l <ayomikesam@gmail.com>
2025-12-11 12:00:52 +0100

    implemented the use of cors

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express, { json, urlencoded } from "express";
 import { router } from "./routes/route.js";
+import cors from "cors";
 
 const PORT = 5050;
 export const app = express(); //instance of my backend application
 app.use(urlencoded({ extended: true }));
 app.use(json()); //to recognize the incoming Request Object as a JSON Object
+app.use(
+  cors({ optionsSuccessStatus: 200, origin: "*", credentials: "include" })
+);
 app.use("/api", router); //localhost:5050/api
 
 app.listen(PORT, () => {
