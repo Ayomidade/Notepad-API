8bfb8b34c28a432569697de5231f413ddd177e7d
myk3l <ayomikesam@gmail.com>
2025-12-12 01:19:53 +0100

    made changes to the routes endpoint

--- routes/route.js
+++ routes/route.js
@@ -20,10 +20,10 @@ router.get("/root", defaultHandler);
 router.post("/new-user", newUser); //API to create new user(working)
 router.post("/login", rateLimiter, loginUser); //API to login user (working)
 router.post("/user", auth, currentUser);
-router.post("/create-note", auth, createNoteHandler); // API to create a new note (working)
+router.post("/notes", auth, createNoteHandler); // API to create a new note (working)
 router.get("/notes", auth, getNotesHandler); // API to get all notes of a user (working)
-router.delete("/clear-note/:id", auth, deleteNoteHandler); //API to delete a single note with the notedId and userId
-router.get("/get-note/:id", auth, getSingleNoteHandler); //API to get a single note with the noteId and userId
+router.delete("/notes/:id", auth, deleteNoteHandler); //API to delete a single note with the notedId and userId
+router.get("/notes/:id", auth, getSingleNoteHandler); //API to get a single note with the noteId and userId
 router.patch("/update-note/:id", auth, updateNoteHandler);
 
 // export default router;
